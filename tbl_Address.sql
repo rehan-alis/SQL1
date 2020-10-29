6e4f0e3cde6a38dec0e5db3f049ed267a1635b14
DESKTOP-RLMUFAU\Reali <rehan.ali@pral.com.pk>
2020-10-29 22:48:37 +0500

    Remove Foreign keys

--- tbl_Address.sql
+++ tbl_Address.sql
@@ -1,7 +1,7 @@
-SET ANSI_NULLS ON
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Address]') AND type in (N'U'))
+DROP TABLE [dbo].[Address]
 GO
-SET QUOTED_IDENTIFIER ON
-GO
+
 CREATE TABLE [dbo].[Address](
 	[ID] [bigint] IDENTITY(1,1) NOT NULL,
 	[NumStreetPOBox] [varchar](255) NULL,
@@ -14,19 +14,4 @@ CREATE TABLE [dbo].[Address](
 	[ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [COMDAT001]
 ) ON [COMDAT001]
-GO
-ALTER TABLE [dbo].[Address]  WITH CHECK ADD  CONSTRAINT [FK_City_Address] FOREIGN KEY([CityID])
-REFERENCES [dbo].[City] ([ID])
-GO
-ALTER TABLE [dbo].[Address] CHECK CONSTRAINT [FK_City_Address]
-GO
-ALTER TABLE [dbo].[Address]  WITH CHECK ADD  CONSTRAINT [FK_Country_Address] FOREIGN KEY([CountryCode])
-REFERENCES [dbo].[Country] ([CountryCode])
-GO
-ALTER TABLE [dbo].[Address] CHECK CONSTRAINT [FK_Country_Address]
-GO
-ALTER TABLE [dbo].[Address]  WITH CHECK ADD  CONSTRAINT [FK_CountrySubEntity_Address] FOREIGN KEY([CountrySubEntityCode])
-REFERENCES [dbo].[CountrySubEntity] ([SubEntityCode])
-GO
-ALTER TABLE [dbo].[Address] CHECK CONSTRAINT [FK_CountrySubEntity_Address]
 GO
